de1f44df88df1863af11bc3bdac1a991eaceced6
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2025-05-20 20:25:44 -0300

    Add Formulario de transação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./components/banner/banner.component";
+import { FormNovaTransacaoComponent } from "./components/form-nova-transacao/form-nova-transacao.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [BannerComponent, FormNovaTransacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/form-nova-transacao/form-nova-transacao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-nova-transacao',
+  imports: [],
+  templateUrl: './form-nova-transacao.component.html',
+  styleUrl: './form-nova-transacao.component.css'
+})
+export class FormNovaTransacaoComponent {
+
+}

a7bc8654449fbef8cd32621b07f839fe108a0a42
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2025-05-20 20:37:29 -0300

    Implementando metodologia 'two-way date biding'

--- src/app/components/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/components/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
+  valorTransacao = "10";
 
+  aoSubimeter(){
+    console.log(this.valorTransacao)
+  }
 }
